b1c2612e9eac26bda728a3b2e7e143ccf42581d8
Chi-Chidev <fabulousesther05@gmail.com>
2024-08-19 15:55:54 +0100

    created an html file, css file, a javaScript file  and a readme file

---
+++ README.md
@@ -0,0 +1,2 @@
+this is a my second java script project. 
+the tools i used here are : HTML, CSS, JavaScript.
---
+++ employees.js
@@ -0,0 +1,189 @@
+document.getElementById('salary1').onclick = function () {
+
+    const position = document.getElementById('position1').value.toLowerCase();
+
+    let salaryMessage;
+
+    if (position === "manager") {
+        salaryMessage = "This employee's salary is $5,000.";
+    } else if (position === "developer") {
+        salaryMessage = "This employee's salary is $3,000.";
+    } else if (position === "intern") {
+        salaryMessage = "This employee's salary is $1,000.";
+    } else {
+        salaryMessage = "Invalid position. Please enter 'Manager', 'Developer', or 'Intern'.";
+    }  
+
+    alert(salaryMessage);
+}
+
+document.getElementById('salary2').onclick = function () {
+
+    const position = document.getElementById('position2').value.toLowerCase();
+
+    let salaryMessage;
+
+    if (position === "manager") {
+        salaryMessage = "This employee's salary is $5,000.";
+    } else if (position === "developer") {
+        salaryMessage = "This employee's salary is $3,000.";
+    } else if (position === "intern") {
+        salaryMessage = "This employee's salary is $1,000.";
+    } else {
+        salaryMessage = "Invalid position. Please enter 'Manager', 'Developer', or 'Intern'.";
+    }  
+
+    alert(salaryMessage);
+}
+
+document.getElementById('salary3').onclick = function () {
+
+    const position = document.getElementById('position3').value.toLowerCase();
+
+    let salaryMessage;
+
+    if (position === "manager") {
+        salaryMessage = "This employee's salary is $5,000.";
+    } else if (position === "developer") {
+        salaryMessage = "This employee's salary is $3,000.";
+    } else if (position === "intern") {
+        salaryMessage = "This employee's salary is $1,000.";
+    } else {
+        salaryMessage = "Invalid position. Please enter 'Manager', 'Developer', or 'Intern'.";
+    }  
+
+    alert(salaryMessage);
+}
+
+document.getElementById('salary4').onclick = function () {
+
+    const position = document.getElementById('position4').value.toLowerCase();
+
+    let salaryMessage;
+
+    if (position === "manager") {
+        salaryMessage = "This employee's salary is $5,000.";
+    } else if (position === "developer") {
+        salaryMessage = "This employee's salary is $3,000.";
+    } else if (position === "intern") {
+        salaryMessage = "This employee's salary is $1,000.";
+    } else {
+        salaryMessage = "Invalid position. Please enter 'Manager', 'Developer', or 'Intern'.";
+    }  
+
+    alert(salaryMessage);
+}
+
+document.getElementById('salary5').onclick = function () {
+
+    const position = document.getElementById('position5').value.toLowerCase();
+
+    let salaryMessage;
+
+    if (position === "manager") {
+        salaryMessage = "This employee's salary is $5,000.";
+    } else if (position === "developer") {
+        salaryMessage = "This employee's salary is $3,000.";
+    } else if (position === "intern") {
+        salaryMessage = "This employee's salary is $1,000.";
+    } else {
+        salaryMessage = "Invalid position. Please enter 'Manager', 'Developer', or 'Intern'.";
+    }  
+
+    alert(salaryMessage);
+}
+
+document.getElementById('salary6').onclick = function () {
+
+    const position = document.getElementById('position6').value.toLowerCase();
+
+    let salaryMessage;
+
+    if (position === "manager") {
+        salaryMessage = "This employee's salary is $5,000.";
+    } else if (position === "developer") {
+        salaryMessage = "This employee's salary is $3,000.";
+    } else if (position === "intern") {
+        salaryMessage = "This employee's salary is $1,000.";
+    } else {
+        salaryMessage = "Invalid position. Please enter 'Manager', 'Developer', or 'Intern'.";
+    }  
+
+    alert(salaryMessage);
+}
+
+document.getElementById('salary7').onclick = function () {
+
+    const position = document.getElementById('position7').value.toLowerCase();
+
+    let salaryMessage;
+
+    if (position === "manager") {
+        salaryMessage = "This employee's salary is $5,000.";
+    } else if (position === "developer") {
+        salaryMessage = "This employee's salary is $3,000.";
+    } else if (position === "intern") {
+        salaryMessage = "This employee's salary is $1,000.";
+    } else {
+        salaryMessage = "Invalid position. Please enter 'Manager', 'Developer', or 'Intern'.";
+    }  
+
+    alert(salaryMessage);
+}
+
+document.getElementById('salary8').onclick = function () {
+
+    const position = document.getElementById('position8').value.toLowerCase();
+
+    let salaryMessage;
+
+    if (position === "manager") {
+        salaryMessage = "This employee's salary is $5,000.";
+    } else if (position === "developer") {
+        salaryMessage = "This employee's salary is $3,000.";
+    } else if (position === "intern") {
+        salaryMessage = "This employee's salary is $1,000.";
+    } else {
+        salaryMessage = "Invalid position. Please enter 'Manager', 'Developer', or 'Intern'.";
+    }  
+
+    alert(salaryMessage);
+}
+
+document.getElementById('salary9').onclick = function () {
+
+    const position = document.getElementById('position9').value.toLowerCase();
+
+    let salaryMessage;
+
+    if (position === "manager") {
+        salaryMessage = "This employee's salary is $5,000.";
+    } else if (position === "developer") {
+        salaryMessage = "This employee's salary is $3,000.";
+    } else if (position === "intern") {
+        salaryMessage = "This employee's salary is $1,000.";
+    } else {
+        salaryMessage = "Invalid position. Please enter 'Manager', 'Developer', or 'Intern'.";
+    }  
+
+    alert(salaryMessage);
+}
+
+document.getElementById('salary10').onclick = function () {
+
+    let position = document.getElementById('position10').value.toLowerCase();
+
+    let salaryMessage;
+
+    if (position === "manager") {
+        salaryMessage = "This employee's salary is $5,000.";
+    } else if (position === "developer") {
+        salaryMessage = "This employee's salary is $3,000.";
+    } else if (position === "intern") {
+        salaryMessage = "This employee's salary is $1,000.";
+    } else {
+        salaryMessage = "Invalid position. Please enter 'Manager', 'Developer', or 'Intern'.";
+    }  
+
+    alert(salaryMessage);
+}
